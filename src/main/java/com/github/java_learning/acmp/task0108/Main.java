8ff4547d27a8e6b7864add2b65314afd0fae2b18
Roman Nechay <chemistroma@yandex.ru>
2022-01-19 20:12:34 +0300

    Solved ACMP 0108

---
+++ src/main/java/com/github/java_learning/acmp/task0108/Main.java
@@ -0,0 +1,19 @@
+package com.github.java_learning.acmp.task0108;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+
+        int a = in.nextInt();
+
+        int b = a;
+        out.print(b);
+        out.flush();
+    }
+}
+
+
