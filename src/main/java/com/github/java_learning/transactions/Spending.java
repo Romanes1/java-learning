7359319a0aa11d70b2b2ec83672ea8df01668cfc
Roman Nechay <chemistroma@yandex.ru>
2022-04-09 12:45:35 +0300

    Made Override for equals, hashcode and toString for transactions

--- src/main/java/com/github/java_learning/transactions/Spending.java
+++ src/main/java/com/github/java_learning/transactions/Spending.java
@@ -13,6 +13,28 @@ public class Spending extends Transaction {
         return -amount;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Spending spending = (Spending) o;
+
+        return Double.compare(spending.amount, amount) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        long temp = Double.doubleToLongBits(amount);
+        return (int) (temp ^ (temp >>> 32));
+    }
+
+    @Override
+    public String toString() {
+        return "Spending{" +
+                "amount=" + amount +
+                '}';
+    }
 }
 
 
